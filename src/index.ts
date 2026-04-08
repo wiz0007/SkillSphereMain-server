035d1848cebc07a8e8544412491dca47db08d76a
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2026-04-09 00:25:32 +0530

    notification feature

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
-
-dotenv.config({
-  path: path.resolve(process.cwd(), ".env")
-});
 import cors from "cors";
 import express from "express";
+import http from "http";
 
 import { connectDB } from "./config/db.js";
+import { initSocket } from "./config/socket.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import profileRoutes from "./routes/profile.routes.js";
@@ -15,30 +13,35 @@ import sessionRoutes from "./routes/session.routes.js";
 import activityRoutes from "./routes/activity.routes.js";
 import courseRoutes from "./routes/course.routes.js";
 
+import { configureCloudinary } from "./config/cloudinary.js";
 
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
 
 connectDB();
-
-import { configureCloudinary } from "./config/cloudinary.js";
-configureCloudinary();  
+configureCloudinary();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-console.log("ENV TEST:", process.env.CLOUD_NAME);
-
+/* ROUTES */
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
-
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/activity", activityRoutes);
-
 app.use("/api/courses", courseRoutes);
+
+/* ✅ CREATE HTTP SERVER */
+const server = http.createServer(app);
+
+/* ✅ INIT SOCKET */
+initSocket(server);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
